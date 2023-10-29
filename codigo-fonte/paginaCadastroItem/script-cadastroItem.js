cc9868e77f33c5489d0445f9a1a3c425f7fccddd
Gabriela Bretas <146037128+gabbibretas@users.noreply.github.com>
2023-10-29 12:37:52 -0300

    leitura de itens salvos na listagem
    botões em funcionamento.

--- codigo-fonte/paginaCadastroItem/script-cadastroItem.js
+++ codigo-fonte/paginaCadastroItem/script-cadastroItem.js
@@ -55,7 +55,8 @@ function cadastrar() {
         historia: historia.value,
         avaliacao: avaliacao.value,
         origem: origem.value,
-        imagem: imagem.value
+        imagem: selectedImageBase64,
+        categoria: categoria.value 
       });
   
     localStorage.setItem("listaItens", JSON.stringify(listaItens))
@@ -63,6 +64,28 @@ function cadastrar() {
     alert("Item Cadastrado com Sucesso!");    
 
     setTimeout(function() {
-        window.location.reload();
-    }, 2000);
-  }
+      window.location.href='../paginaMinhasColecoes/minhaColecao.html';
+    }, 1000);
+}
+const imageInput = document.getElementById("imagem");
+
+imageInput.addEventListener("change", handleImageSelection);
+
+let selectedImageBase64 = null;
+
+function handleImageSelection() {
+    const selectedFile = imageInput.files[0];
+    if (selectedFile) {
+        const reader = new FileReader();
+
+        reader.onload = function (event) {
+            selectedImageBase64 = event.target.result;
+
+            const imgElement = document.getElementById("imagemSelecionada");
+            imgElement.src = selectedImageBase64;
+          };
+          
+        reader.readAsDataURL(selectedFile);
+    }
+}
+
