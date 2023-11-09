032935e45397b83a02cf6a2e1343a2e784f3cac5
Lucas Souza <90305152+lucasbenacci@users.noreply.github.com>
2023-11-09 12:04:46 -0300

    Update script-cadastroItem.js

--- codigo-fonte/js/script-cadastroItem.js
+++ codigo-fonte/js/script-cadastroItem.js
@@ -19,13 +19,8 @@ function cadastrar() {
     listaItens.push(
       {
         tituloDoItem: tituloDoItem.value,
-        descricao: descricao.value,
-        dataAquisicao: dataAquisicao.value,
-        historia: historia.value,
-        avaliacao: avaliacao.value,
-        origem: origem.value,
         imagem: selectedImageBase64,
-        categoria: categoria.value 
+        categoria: categoria.value, 
       });
   
     localStorage.setItem("listaItens", JSON.stringify(listaItens))
