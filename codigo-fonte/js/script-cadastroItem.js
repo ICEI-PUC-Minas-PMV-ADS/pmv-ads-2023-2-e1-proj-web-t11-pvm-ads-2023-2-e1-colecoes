ab03075a93b5ecd0d7a49ff6df198299acb7cee4
lucasbenacci <90305152+lucasbenacci@users.noreply.github.com>
2023-11-09 13:35:44 -0300

    Update

--- codigo-fonte/js/script-cadastroItem.js
+++ codigo-fonte/js/script-cadastroItem.js
@@ -14,31 +14,22 @@ function cadastrar() {
     
      
   
-
     let listaItens = JSON.parse(localStorage.getItem('listaItens') || '[]')
   
     listaItens.push(
       {
         tituloDoItem: tituloDoItem.value,
-        descricao: descricao.value,
-        dataAquisicao: dataAquisicao.value,
-        historia: historia.value,
-        avaliacao: avaliacao.value,
-        origem: origem.value,
         imagem: selectedImageBase64,
-        categoria: categoria.value 
+        categoria: categoria.value, 
       });
   
     localStorage.setItem("listaItens", JSON.stringify(listaItens))
   
     alert("Item Cadastrado com Sucesso!");    
-
     setTimeout(function() {
       window.location.href='../pages/minhacolecao.html';
     }, 1000);
 }
-<<<<<<< Updated upstream
-=======
 const imageInput = document.getElementById("imagem");
 
 imageInput.addEventListener("change", handleImageSelection);
@@ -60,4 +51,3 @@ function handleImageSelection() {
         reader.readAsDataURL(selectedFile);
     }
 }
->>>>>>> Stashed changes
